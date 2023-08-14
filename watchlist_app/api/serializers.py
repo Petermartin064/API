91b519c8826008bd05e25017cd55d398f1a5a0a5
Peter Martin <petermartin602@gmail.com>
2023-08-14 11:14:17 +0300

    Validators

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def name_Length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name must be at least 2 characters long")
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_Length])
     description = serializers.CharField()
     active = serializers.BooleanField()   
     
@@ -16,3 +19,16 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    #Object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Name and Description cannot be the same")
+        return data
+    
+    # #Field level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name must be at least 2 characters long")
+    #     else:
+    #         return value
