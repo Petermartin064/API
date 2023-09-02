7d2b35b1962b04712fb80ff718ee2e12f71edd06
Peter Martin <petermartin602@gmail.com>
2023-09-02 11:08:49 +0300

    Automatic deletion of Token during logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -6,6 +6,14 @@ from rest_framework.authtoken.models import Token
 from user_app.api.serializers import RegistrationSerializer
 from user_app import models
 
+
+@api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+
 @api_view(['POST'])
 def Registration_view(request):
     if request.method == 'POST':
