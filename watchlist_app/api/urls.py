060fb0a1c26622eea41e48d08d15cef02863174b
Peter Martin <petermartin602@gmail.com>
2023-09-25 10:13:34 +0300

    Testcases for all methods in review

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),
     # path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('', include(router.urls)),
-    path('<int:pk>/review/', ReviewList.as_view(), name='streamplatform-detail'),
+    path('<int:pk>/review/', ReviewList.as_view(), name='review-list'),
     path('<int:pk>/createreview/', ReviewCreate.as_view(), name='review-create'),
     path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
     path('review/', UserReview.as_view(), name='user-review'),
